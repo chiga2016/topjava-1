5574ca58ee707d324faeeca7fd33a2a0b4258b33
BalagutdinovIF <chigachiga2011@gmail.com>
2021-06-21 00:22:09 +0500

    working view

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+insert into meals ( user_id, date_time, description, calories)
+values (100000, to_date('20.06.2021','dd.mm.yyyy'), 'zavtrak', 500 ),
+       (100000, to_date('20.06.2021','dd.mm.yyyy'), 'obed', 1500 ),
+       (100000, to_date('20.06.2021','dd.mm.yyyy'), 'ujin', 200 ),
+       (100001, to_date('21.06.2021','dd.mm.yyyy'), 'obed', 500 ),
+       (100000, to_date('21.06.2021','dd.mm.yyyy'), 'zavtrak', 800 ),
+       (100000, to_date('21.06.2021','dd.mm.yyyy'), 'obed', 1800 ),
+       (100000, to_date('21.06.2021','dd.mm.yyyy'), 'ujin', 900 );
